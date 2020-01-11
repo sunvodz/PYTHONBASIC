e13e49a6689f03ad244c35301764011cca00330f
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-12 04:07:10 +0700

    Ex5

--- Ex5/Ex5.py
+++ Ex5/Ex5.py
@@ -3,5 +3,6 @@ age = 22
 
 print(" My name is {0} \n I'm {1} year old ".format(name,age))
 
-
 print(" My name is {names} \n I'm {ages} year old ".format(names=name,ages=age))
+
+print(" My name is {name} \n I'm {age} year old ".format(**locals()))
