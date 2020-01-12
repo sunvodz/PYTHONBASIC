115babc7107609225daeb45f2da5697f57721b1b
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-12 10:22:49 +0700

    Ex9

---
+++ Ex9/Ex9.py
@@ -0,0 +1,46 @@
+a = 10
+#== , != , < , > , <= , >=
+if a < 20:
+  print('yes')
+else:
+  print('no')
+  
+# order = {'50':'beer','10':'water','20':'lemon'}
+
+# select =  input("Select Order : ")
+# if order[select] == 'beer':
+#   print('Happy')
+# elif order[select] == 'water':
+#   print('Happy')
+# elif order[select] == 'lemon':
+#   print('Happy')
+  
+  
+point = int(input("Enter your point : "))
+if point >= 80:
+  if point >= 100:
+    print("Over Limit point is 100");
+  else:
+    print("Grade A")
+elif point >= 75:
+  print("Grade B+")
+elif point >= 70:
+  print("Grade B")
+elif point >= 65:
+  print("Grade C+")
+elif point >= 60:
+  print("Grade C")
+elif point >= 55:
+  print("Grade D+")
+elif point >= 50:
+  print("Grade D")
+else:
+  if point < 0:
+    print("Please Enter number more 0")
+  else:
+    print("Grade F")
+  
+  
+  
+
+
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Ex5 : String and Argument
 Ex6 : Sequence tuple , list , dict
 Ex7 : Use sequence list
 Ex8 : Input and cover type
+Ex9 : If Else
 ```
 
 ##SunvoDz##
