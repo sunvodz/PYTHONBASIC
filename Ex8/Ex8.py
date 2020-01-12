e70fdef9281dd64916b0934bd0fdc5142b926cff
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-12 09:21:57 +0700

    Ex8

---
+++ Ex8/Ex8.py
@@ -0,0 +1,17 @@
+number = input('Enter number : ')
+
+print(number)
+
+print(type(number))  #string
+
+print(123+int(number)) #cover string to int and operator
+
+
+
+number = int(input('Enter number : '))
+
+print(number)
+
+print(type(number))  #int
+
+print(123+number) #operator
