72d25ca21d74b7bb633a8b389ba603c832e54a5e
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-12 08:52:35 +0700

    Ex6

---
+++ Ex6/Ex6.py
@@ -0,0 +1,29 @@
+#Sequence tuple
+name_a = ('sunvo','pongpan','SunvoDz')
+
+print(name_a)
+
+print(name_a[2])
+
+print(type(name_a))  #tuple
+
+#Sequence list
+name_b = ['sunvo','pongpan','SunvoDz']
+
+print(name_b)
+
+print(name_b[2])
+
+print(type(name_b)) #list
+
+#Sequence dict
+name_c = {10:'sunvo',20:'pongpan',30:'SunvoDz'}
+
+print(name_c)
+
+print(name_c[20])
+
+print(name_c[20], name_c[30])
+
+print(type(name_c)) #dict
+
