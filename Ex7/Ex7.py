71de2f5ffdcdd45aac9005d8da175e910f3b1093
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-12 09:14:32 +0700

    Ex7

---
+++ Ex7/Ex7.py
@@ -0,0 +1,28 @@
+#Use sequence list
+name_b = ['sunvo','pongpan','SunvoDz']
+
+name_b.append('Mahasarakham') #add last
+
+print(name_b) 
+
+name_b.insert(0,'Tn.Group') #add first
+
+print(name_b)
+
+name_b.pop() #Delete last
+
+print(name_b)
+
+name_b.pop(1) #Delete array[1]
+
+print(name_b)
+
+number = ['0','1','2','5','4']
+
+number.sort()
+
+print(number) 
+
+number.reverse()
+
+print(number) 
